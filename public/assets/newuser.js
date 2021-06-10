1c6bd53d29d85fb11e58827cf64c92ab6a084fbd
Alex Bradshaw <alexanderbradshaw5@gmail.com>
2021-06-10 16:18:01 -0400

    new user functionality added

--- public/assets/newuser.js
+++ public/assets/newuser.js
@@ -2,7 +2,7 @@ async function newFormHandler(event) {
     event.preventDefault();
     const email = document.getElementById('email-login').value;
     const password = document.getElementById('password-login').value;
-
+console.log(`${email} \n ${password}`);
     const response = await fetch(`/api/users/signup`, {
         method: 'POST',
         body: JSON.stringify({
@@ -15,6 +15,8 @@ async function newFormHandler(event) {
     });
     console.log(response);
     if (response.ok) {
+        email.value = null
+        password.value = null
         document.location.replace('/login');
     } else {
         alert('Failed to add user');
